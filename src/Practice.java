33decd2201376ebab2470403a95a03c7726376c9
Jake-G123 <jakelgerken@gmail.com>
2026-01-13 14:46:20 -0800

    Finished livecode

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,18 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node head = new Node('g', null);
+        Node ryan = new Node('s', null);
+        head.next = ryan;
+        ryan.next = new Node('t', null);
+        ryan.next.next = new Node('x', null);
+
+        printList(head);
+    }
+
+    public static void printList(Node current) {
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
     }
 }
