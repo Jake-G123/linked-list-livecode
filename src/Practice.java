8b7c8543831fd279be74ac5817239bb736d35b74
Jake-G123 <jakelgerken@gmail.com>
2026-01-20 14:51:33 -0800

    finished livecode

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,6 @@
 public class Practice {
     public static void main(String[] args) {
+        /*
         Node head = new Node('g', null);
         Node ryan = new Node('s', null);
         head.next = ryan;
@@ -10,5 +11,21 @@ public class Practice {
         System.out.println(contains(head,'x'));
         System.out.println(remove(head, 't'));
         printList(head);
+        
+        myLL<Character> list = new myLL<>();
+        list.addToBack('x');
+        list.addToBack('m');
+        list.addToBack('v');
+        list.addToBack('q');
+        list.printList();
+        */
+        myLL<Integer> intList = new myLL<>();
+        intList.addToBack(5);
+        intList.addToBack(10);
+        intList.addToBack(25);
+        intList.addToBack(400);
+
+        //intList.printList();
+        intList.printBackwards();
     }
 }
--- src/myLL.java
+++ src/myLL.java
@@ -1,32 +1,66 @@
-public class myLL {
-    private class Node {
-        public char value;
-        public Node next;
+public class myLL<T> {
+    private class Node<E> {
+        public E value;
+        public Node<E> next;
+        public Node<E> previous;
         
-        public Node(char value, Node next) {
+        public Node(E value, Node<E> next, Node<E> previous) {
             this.value = value;
             this.next = next;
+            this.previous = previous;
+            if(this.next != null) {
+                this.next.previous = this;
+            }
+            if (this.previous != null) {
+                this.previous.next = this;
+            }
         }
     }
 
-    private Node head;
+    private Node<T> head;
+    private Node<T> tail;
 
     public myLL() {
         head = null;
+        tail = null;
     }
 
     public void printList() {
-        Node current = head;
+        Node<T> current = head;
         while (current != null) {
             System.out.println(current.value);
             current = current.next;
         }
     }
 
-    public boolean contains(char toFind) {
-        Node start = head;
+    public void printBackwards() {
+        if (tail == null) return;
+        Node<T> current = tail;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.previous;
+        }
+
+    }
+
+    public void addToBack(T toAdd) {
+        if (tail != null) {
+            Node<T> newNode = new Node<>(toAdd, null, tail);
+            tail = newNode;
+            if (tail.previous.previous == null) {
+                head = tail.previous;
+            }
+        } else {
+            Node<T> newNode = new Node<>(toAdd, null, null);
+            head = newNode;
+            tail = newNode;
+        }
+    }
+
+    public boolean contains(T toFind) {
+        Node<T> start = head;
         while (start != null) {
-            if (start.value == toFind) {
+            if (start.value.equals(toFind)) {
                 return true;
             }
             start = start.next;
@@ -35,23 +69,28 @@ public class myLL {
     }
 
     // remove the first node that has toRemove
-    public char remove(char toRemove) {
-        Node start = head;
-        if (start == null) {
-            return '\0';
+    public T remove(T toRemove) {
+        if (head.value.equals(toRemove)) {
+            head = head.next;
+            head.previous = null;
+            return toRemove;
         }
-        if (start.value == toRemove) {
-
+        Node<T> start = head;
+        if (start == null) {
+            return null;
         }
         while (start.next != null) {
-            char removed = start.next.value;
-            if (removed == toRemove) {
+            T removed = start.next.value;
+            if (removed.equals( toRemove)) {
                 start.next = start.next.next;
+                start.next.previous = start.next;
                 return removed;
             }
-            head = start.next;
+            start.previous = start;
+            start = start.next;
+
         }
-        return '\0';
+        return null;
     }
 
 }
